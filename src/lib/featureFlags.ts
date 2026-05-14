812b02b1f96f20b18ed07d47293a6e858c99ac11
Gowreesh V T <135145326+Gowreesh-VT@users.noreply.github.com>
2026-05-14 12:55:37 +0530

    chore: clean up feature flags configuration

--- src/lib/featureFlags.ts
+++ src/lib/featureFlags.ts
@@ -1,5 +1,5 @@
 export const FEATURE_FLAGS = {
-  betaTimetableFlow: 'beta_timetable_flow',
+  facultyFirstPreferenceFlow: 'faculty_first_preference_flow',
 } as const;
 
 export type FeatureFlagName = typeof FEATURE_FLAGS[keyof typeof FEATURE_FLAGS];
